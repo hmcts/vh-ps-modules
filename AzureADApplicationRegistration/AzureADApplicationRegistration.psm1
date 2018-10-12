1c6cc546d547e7979567ab61d3d5b8a8ddf7a0e7
Janis Berzins <janis.berzins@hotmail.co.uk>
2018-10-12 14:22:20 +0300

    New function to add IdentifierUris

--- AzureADApplicationRegistration/AzureADApplicationRegistration.psm1
+++ AzureADApplicationRegistration/AzureADApplicationRegistration.psm1
@@ -222,4 +222,27 @@ function Set-AzureADResourceAccess {
     }
 }
 
-Export-ModuleMember -Function 'Invoke-AzureADApplicationRegistration', 'Invoke-AzureConnection', 'Set-AzureADResourceAccess'
+# Add additional identifierUris to Azure AD app
+function New-IdentifierUris {
+    param (
+        [String] 
+        [Parameter(Mandatory)]
+        $AADAppName,
+        [String]
+        [ValidateScript({[system.uri]::IsWellFormedUriString($_,[System.UriKind]::Absolute)})]
+        [Parameter(Mandatory)]
+        $URI
+    )
+    $AzureADApp = Get-AzureADApplication -SearchString $AADAppName
+    if ($AzureADApp.IdentifierUris -notcontains $URI) {
+        $IdentifierUris = $null
+        $IdentifierUris = $AzureADApp.IdentifierUris
+        $IdentifierUris.add($URI)
+        Set-AzureADApplication -ObjectId $AzureADApp.ObjectId -IdentifierUris $IdentifierUris
+    }
+    else {
+        Write-Output "Replay URI is present"
+    }   
+}
+
+Export-ModuleMember -Function 'Invoke-AzureADApplicationRegistration', 'Invoke-AzureConnection', 'Set-AzureADResourceAccess', 'New-IdentifierUris'

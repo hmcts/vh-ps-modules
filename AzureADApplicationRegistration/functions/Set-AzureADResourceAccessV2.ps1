a18aec878940ca62c4d1e54a4be68d0a1b908858
berzns <janis.berzins@outlook.co.uk>
2019-02-05 11:21:41 +0000

    Cleaned up the script

--- AzureADApplicationRegistration/functions/Set-AzureADResourceAccessV2.ps1
+++ AzureADApplicationRegistration/functions/Set-AzureADResourceAccessV2.ps1
@@ -21,15 +21,14 @@ function Set-AzureADResourceAccessV2 {
         }
 
         # new object for setting RequiredResourceAccess for client app
-        $RequiredResourceAccessObject = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+        $requiredResourceAccessObject = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
 
         # Create new variable with existing permissions
-        $azureADAppExistingRequiredResourceAccess = $AzureADAppClient.RequiredResourceAccess
+        $azureADAppExistingRequiredResourceAccess = $azureADAppClient.RequiredResourceAccess
 
     }
     
     process {
-
         foreach ($resource in $apiAccessJSON.requiredResourceAccess) {
             Write-Output   ("Checking if Required Resource Name '{0}' with Required Resource Id: '{1}' has been set..." -f $resource.resourceAppName, $resource.resourceAppId)
 
@@ -38,55 +37,54 @@ function Set-AzureADResourceAccessV2 {
                 foreach ($resourceAccess in $resource.resourceAccess) {
                     Write-Output ("Checking if Required Resource Access ID '{0}' with Required Resource Access Type: '{1}' has been set..." -f $resourceAccess.id, $resourceAccess.type)
                     if ($azureADAppClient.RequiredResourceAccess.ResourceAccess.id -notcontains $resourceAccess.id) {
-
-                        # new object for setting RequiredResourceAccess for client app
-                        #$RequiredResourceAccessObject = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+                        Write-Output ("Required Resource Access ID '{0}' with Required Resource Access Type: '{1}' has not been set." -f $resourceAccess.id, $resourceAccess.type)
+                        Write-Output ("Setting up now...")
 
                         # oauth2Permission ID, Used for configuring the client App's "requiredResourceAccess" permissions.
                         $ResourceAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $resourceAccess.id, $resourceAccess.type
         
                         # add values to object
-                        $RequiredResourceAccessObject.ResourceAccess += $ResourceAccess                        
+                        $requiredResourceAccessObject.ResourceAccess += $ResourceAccess                        
                     }
                 }
                 # Add Required Resource App ID thats is missing to the object.
-                $RequiredResourceAccessObject.ResourceAppId = $resource.resourceAppId
-                $azureADAppExistingRequiredResourceAccess.add($RequiredResourceAccessObject)
+                $requiredResourceAccessObject.ResourceAppId = $resource.resourceAppId
+                $azureADAppExistingRequiredResourceAccess.add($requiredResourceAccessObject)
             }
             else {
+                Write-Output   ("Required Resource '{0}' with Required Resource Id: '{1}' has been set." -f $resource.resourceAppName, $resource.resourceAppId)
 
                 foreach ($existingRequiredResource in $azureADAppExistingRequiredResourceAccess) {
                     if ($existingRequiredResource.ResourceAppId -eq $resource.resourceAppId) {
 
                         foreach ($resourceAccess in $resource.resourceAccess) {
                             Write-Output ("Checking if Required Resource Access ID '{0}' with Required Resource Access Type: '{1}' has been set..." -f $resourceAccess.id, $resourceAccess.type)
+                            
                             if ($azureADAppClient.RequiredResourceAccess.ResourceAccess.id -notcontains $resourceAccess.id) {
+                                Write-Output ("Required Resource Access ID '{0}' with Required Resource Access Type: '{1}' has not been set." -f $resourceAccess.id, $resourceAccess.type)
+                                Write-Output ("Setting up now...")
     
-                                # new object for setting RequiredResourceAccess for client app
-                                #$RequiredResourceAccessObject = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
-    
-                                # oauth2Permission ID, Used for configuring the client App's "requiredResourceAccess" permissions.
+                                # New resource access object with values
                                 $ResourceAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $resourceAccess.id, $resourceAccess.type
             
                                 # add values to object
-                                $RequiredResourceAccessObject.ResourceAccess += $ResourceAccess                        
+                                $requiredResourceAccessObject.ResourceAccess += $ResourceAccess                        
                             }
                         }
-
                         # Add the new Resource Access to the Existing resource access object
-                        $existingRequiredResource.ResourceAccess += $RequiredResourceAccessObject.ResourceAccess
+                        if ($null -EQ $requiredResourceAccessObject.ResourceAccess) {
+                            Write-Output "No new Resource Access permissions found to be set."
+                            
+                        }
+                        else {
+                            $existingRequiredResource.ResourceAccess += $requiredResourceAccessObject.ResourceAccess
+                        }
                     }
-                    
                 }
             }
         }
-
-    }
-
-    
+    }    
     end {
-
-        
         Set-AzureADApplication -ObjectId $azureADAppClient.ObjectId -RequiredResourceAccess $azureADAppExistingRequiredResourceAccess
     }
 }

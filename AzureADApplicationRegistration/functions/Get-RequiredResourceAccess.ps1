d83bbe1ae95768eb94e8233c42b70314c3fdbe73
ddikman <david.dikman@hmcts.net>
2019-02-13 10:59:59 +0000

    Added the new script to get required resource accesses from an app

--- AzureADApplicationRegistration/functions/Get-RequiredResourceAccess.ps1
+++ AzureADApplicationRegistration/functions/Get-RequiredResourceAccess.ps1
@@ -10,9 +10,10 @@ function Get-RequiredResourceAccess {
     $Application = Get-AzureADApplication -SearchString $AppName
     $ResourceAccess = $Application.RequiredResourceAccess
 
+    # Create a new list for the output since the output has different properties than the source objects
     $ResultResourceList = @()
-
     foreach ($Resource in $ResourceAccess) {
+        # Find the resource that we require access to
         $ResourceObj = Get-AzureADServicePrincipal -All $true | Where-Object AppId -eq $Resource.ResourceAppId | Select-Object -first 1 -Property AppDisplayName, Oauth2Permissions
         $ResultResourceObject = New-Object PSObject
         $ResultResourceObject | Add-Member -NotePropertyName ResourceAppName -NotePropertyValue $ResourceObj.AppDisplayName
@@ -20,6 +21,7 @@ function Get-RequiredResourceAccess {
 
         $ResultResourceAccesses = @()
         foreach ($RequiredResource in $Resource.ResourceAccess) {
+            # For each required permission, find the permission object and get the name of it
             $Permission = $ResourceObj.Oauth2Permissions | Where-Object Id -eq $RequiredResource.Id | Select-Object -first 1
             $NewResourceAccessObj = New-Object PsObject
             $NewResourceAccessObj | Add-Member -NotePropertyName "ResourceAccessName" -NotePropertyValue $Permission.AdminConsentDisplayName

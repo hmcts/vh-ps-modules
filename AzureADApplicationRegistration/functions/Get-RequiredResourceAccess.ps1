b5c9aa40e20617baad24c8287d1b73e54c11c96a
David Dikman <ddikman@gmail.com>
2019-02-18 11:18:09 +0000

    Updated styling to match the lower case camel we use in current resource files

--- AzureADApplicationRegistration/functions/Get-RequiredResourceAccess.ps1
+++ AzureADApplicationRegistration/functions/Get-RequiredResourceAccess.ps1
@@ -18,22 +18,24 @@ function Get-RequiredResourceAccess {
         # Find the resource that we require access to
         $ResourceObj = Get-AzureADServicePrincipal -All $true | Where-Object AppId -eq $Resource.ResourceAppId | Select-Object -first 1 -Property AppDisplayName, Oauth2Permissions
         $ResultResourceObject = New-Object PSObject
-        $ResultResourceObject | Add-Member -NotePropertyName ResourceAppName -NotePropertyValue $ResourceObj.AppDisplayName
-        $ResultResourceObject | Add-Member -NotePropertyName ResourceAppId -NotePropertyValue $Resource.ResourceAppId
+        $ResultResourceObject | Add-Member -NotePropertyName "resourceAppName" -NotePropertyValue $ResourceObj.AppDisplayName
+        $ResultResourceObject | Add-Member -NotePropertyName "resourceAppId" -NotePropertyValue $Resource.ResourceAppId
 
         $ResultResourceAccesses = @()
         foreach ($RequiredResource in $Resource.ResourceAccess) {
             # For each required permission, find the permission object and get the name of it
             $Permission = $ResourceObj.Oauth2Permissions | Where-Object Id -eq $RequiredResource.Id | Select-Object -first 1
             $NewResourceAccessObj = New-Object PsObject
-            $NewResourceAccessObj | Add-Member -NotePropertyName "ResourceAccessName" -NotePropertyValue $Permission.AdminConsentDisplayName
-            $NewResourceAccessObj | Add-Member -NotePropertyName "Id" -NotePropertyValue $RequiredResource.Id
-            $NewResourceAccessObj | Add-Member -NotePropertyName "Type" -NotePropertyValue $RequiredResource.Type
+            $NewResourceAccessObj | Add-Member -NotePropertyName "resourceAccessName" -NotePropertyValue $Permission.AdminConsentDisplayName
+            $NewResourceAccessObj | Add-Member -NotePropertyName "id" -NotePropertyValue $RequiredResource.Id
+            $NewResourceAccessObj | Add-Member -NotePropertyName "type" -NotePropertyValue $RequiredResource.Type
             $ResultResourceAccesses += $NewResourceAccessObj
         }
-        $ResultResourceObject | Add-Member -NotePropertyName ResourceAccess -NotePropertyValue $ResultResourceAccesses
+        $ResultResourceObject | Add-Member -NotePropertyName "resourceAccess" -NotePropertyValue $ResultResourceAccesses
         $ResultResourceList += $ResultResourceObject
     }
 
-    return $ResultResourceList
+    $Result = New-Object PSObject
+    $Result | Add-Member -NotePropertyName "requiredResourceAccess" -NotePropertyValue $ResultResourceList
+    return $Result
 }
